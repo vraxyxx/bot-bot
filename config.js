a4bb74aaa254a69d35f29bfaea37be97a6bcdee8
vraxyxx <ryantayo57@gmail.com>
2025-06-24 17:19:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  prefix: "", // If you want a prefix like "!", set it here
+  prefix: "!", // If you want a prefix like "!", set it here
   adminIDs: ["61576677958957"], // Facebook UID(s) allowed for admin commands
   botName: "Autobot VRN",
 };
